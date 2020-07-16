6f916c3895bbaa3695c4b7eb6c795b8c6fe0fc57
Israel-Laguan <israellaguan@gmail.com>
2020-07-15 23:09:07 -0500

    feat: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |i|
+    Item.create(title: Faker::Commerce.product_name, price: Faker::Commerce.price(range: 0..1000.0, as_string: true), description: Faker::Marketing.buzzwords)
+end
